6492da1724f2e516d5081aa8aa1d217b7a5039d3
NabilMustofaa <nabilmustofa6@gmail.com>
2023-08-09 02:10:20 +0700

    combining models api on backedn

---
+++ modules/imgGeneration.js
@@ -0,0 +1,18 @@
+
+const tfTask = require('@tensorflow-models/tasks');
+
+const {Storage} = require('@google-cloud/storage')
+const fs = require('fs')
+const path = require('path');
+
+
+const imgGeneration = async (req, res) => {
+
+
+}
+
+module.exports = {
+  imgGeneration
+}
+
+
